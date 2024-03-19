50f93ece3c38d2adb53f54f486cecf5a652993c8
KLubina <155258933+KLubina@users.noreply.github.com>
2024-03-19 21:37:42 +0100

    Delete package.info.java

--- package.info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Das Paket oop.mandelbrot enthält Klassen zur Erzeugung und Darstellung des Mandelbrot-Fraktals.
- * 
- * @author Aleksandar Travanov
- * @version 1.0
- */
-
-package oop.mandelbrot;
