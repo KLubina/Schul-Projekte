7e73448f2d96ccd8bdb500acb45f2ae1687de09c
KLubina <kristian.lubina@outlook.com>
2024-09-27 17:42:56 +0200

    rankrepostory geändert

--- Modul_183_backend/build/generated/sources/annotationProcessor/java/main/com/example/jwt/domain/user/dto/UserMapperImpl.java
+++ Modul_183_backend/build/generated/sources/annotationProcessor/java/main/com/example/jwt/domain/user/dto/UserMapperImpl.java
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-09-27T17:40:35+0200",
+    date = "2024-09-27T17:42:33+0200",
     comments = "version: 1.5.5.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.5.jar, environment: Java 17.0.12 (Amazon.com Inc.)"
 )
 @Component
--- Modul_183_backend/src/main/java/com/example/jwt/domain/rank/RankRepository.java
+++ Modul_183_backend/src/main/java/com/example/jwt/domain/rank/RankRepository.java
@@ -5,9 +5,25 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-
+/**
+ * Repository interface for managing {@link Rank} entities.
+ */
 @Repository
 public interface RankRepository extends ExtendedRepository<Rank> {
+
+    /**
+     * Retrieves a Rank entity based on its title.
+     *
+     * @param title the title of the Rank to retrieve
+     * @return an {@link Optional} containing the found Rank, or empty if not found
+     */
     Optional<Rank> findByTitle(String title);
+
+    /**
+     * Finds the highest Rank that can be achieved with the given number of seeds.
+     *
+     * @param seeds the number of seeds accumulated by the user
+     * @return an {@link Optional} containing the appropriate Rank if available, otherwise empty
+     */
     Optional<Rank> findFirstBySeedsNeededLessThanEqualOrderBySeedsNeededDesc(int seeds);
 }
