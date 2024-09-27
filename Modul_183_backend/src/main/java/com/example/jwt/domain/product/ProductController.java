06202cab2f721477231621f76946b7b9a4eb70ae
KLubina <kristian.lubina@outlook.com>
2024-09-27 17:52:56 +0200

    productController umgeschrieben

--- Modul_183_backend/src/main/java/com/example/jwt/domain/product/ProductController.java
+++ Modul_183_backend/src/main/java/com/example/jwt/domain/product/ProductController.java
@@ -2,7 +2,8 @@ package com.example.jwt.domain.product;
 
 import com.example.jwt.domain.tea.TeaService;
 import com.example.jwt.domain.tea.dto.TeaDTO;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,21 +12,28 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+/**
+ * Controller zur Verwaltung von Produkten (Tees).
+ */
 @RestController
 @RequestMapping("/products")
+@RequiredArgsConstructor
+@Slf4j
 public class ProductController {
 
     private final TeaService teaService;
 
-    @Autowired
-    public ProductController(TeaService teaService) {
-        this.teaService = teaService;
-    }
-
+    /**
+     * Holt alle verfügbaren Tees.
+     *
+     * @return eine ResponseEntity mit einer Liste von TeaDTO-Objekten
+     */
     @PreAuthorize("hasAuthority('CAN_RETRIEVE_PRODUCTS')")
     @GetMapping
     public ResponseEntity<List<TeaDTO>> getAllTeas() {
+        log.info("Abrufen aller Tees");
         List<TeaDTO> teas = teaService.getAllTeas();
+        log.debug("Anzahl der abgerufenen Tees: {}", teas.size());
         return ResponseEntity.ok(teas);
     }
 }
