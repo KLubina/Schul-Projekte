ddd50d38554b25e2313553430f0b75731236cc48
KLubina <kristian.lubina@outlook.com>
2024-09-27 17:26:34 +0200

    teatype geändert
    läuft

--- Modul_183_backend/src/main/java/com/example/jwt/domain/teaType/TeaType.java
+++ Modul_183_backend/src/main/java/com/example/jwt/domain/teaType/TeaType.java
@@ -2,58 +2,39 @@ package com.example.jwt.domain.teaType;
 
 import com.example.jwt.core.generic.ExtendedEntity;
 import com.example.jwt.domain.tea.Tea;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
-import java.util.HashSet;
 
 @Entity
 @Table(name = "tea_type")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class TeaType extends ExtendedEntity {
 
     @Column(name = "name", nullable = false, unique = true)
     private String name;
 
     @Column(name = "minAge", nullable = true)
-    private int minAge;
+    private Integer minAge;  // Verwenden Sie Integer statt int für Null-Werte
 
-    @OneToMany(mappedBy = "teaType")
+    @OneToMany(mappedBy = "teaType", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
     private Set<Tea> teas = new HashSet<>();
 
-    public TeaType() {
+    // Zusätzliche Methoden (falls erforderlich)
+    public void addTea(Tea tea) {
+        teas.add(tea);
+        tea.setTeaType(this);
     }
 
-    public TeaType(UUID id, String name, Integer minAge, Set<Tea> teas) {
-        super(id);
-        this.name = name;
-        this.minAge = minAge;
-        this.teas = teas;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getMinAge() {
-        return minAge;
-    }
-
-    public void setMinAge(Integer minAge) {
-        this.minAge = minAge;
-    }
-
-    public Set<Tea> getTeas() {
-        return teas;
-    }
-
-    public void setTeas(Set<Tea> teas) {
-        this.teas = teas;
+    public void removeTea(Tea tea) {
+        teas.remove(tea);
+        tea.setTeaType(null);
     }
 }
